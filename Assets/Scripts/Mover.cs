211daeffffed167d2e9886a1da207dbd21c8cc86
Daniel <danjfreire@gmail.com>
2024-12-15 14:52:21 -0300

    feat: add simple input handling with old input system

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,14 +2,21 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    private const string HORIZONTAL_AXIS = "Horizontal";
+    private const string VERTICAL_AXIS = "Vertical";
 
-    private void Start()
-    {
-    }
+    [SerializeField] private float moveSpeed = 10f;
 
     private void Update()
     {
-        transform.Translate(0.01f, 0f, 0f);
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        float xMovement = Input.GetAxis(HORIZONTAL_AXIS) * Time.deltaTime * moveSpeed;
+        float zMovement = Input.GetAxis(VERTICAL_AXIS) * Time.deltaTime * moveSpeed;
+        transform.Translate(xMovement, 0f, zMovement);
     }
 
 }
